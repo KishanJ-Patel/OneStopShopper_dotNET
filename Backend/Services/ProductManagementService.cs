0590702ee21d19fcb4b92160659f0b0f2e9c286e
Kishan J Patel <kishan.patel2002@outlook.com>
2023-10-28 17:54:48 -0600

    docs: Add doc comments for product management service methods

--- Backend/Services/ProductManagementService.cs
+++ Backend/Services/ProductManagementService.cs
@@ -1,9 +1,13 @@
 ﻿using System.Data.Common;
 using Backend.Data;
 using Backend.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Services
 {
+    /// <summary>
+    /// Class <c>ProductManagementService</c> manages products in the database.
+    /// </summary>
     public class ProductManagementService
     {
         public ProductManagementService(OssDbContext context)
@@ -13,6 +17,27 @@ namespace Backend.Services
 
         private readonly OssDbContext _developDbContext;
 
+        // Service Methods
+
+        /// <summary>
+        /// Method <c>SaveProduct</c> saves a product model instance into the database.
+        /// For more information check <c>Services.md</c> doc.
+        /// <example>
+        /// Usage Example:
+        /// <code>
+        /// Product newProduct = new Product
+        /// {
+        ///     Name = "Example Product",
+        ///     Price = 29.99,
+        ///     ImageUri = "https://example.com/images/example-product.jpg"
+        /// };
+        /// SaveProduct(newProduct);
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <exception cref="Exception">
+        /// </exception>
+        /// <param name="product">the product model instance.</param>
         public void SaveProduct(Product product)
         {
             try
@@ -26,8 +51,22 @@ namespace Backend.Services
             }
         }
 
+        /// <summary>
+        /// Method <c>GetProducts</c> copies all products from the database into an array.
+        /// For more information check <c>Services.md</c> doc.
+        /// <example>
+        /// Usage Example:
+        /// <code>
+        /// Product[] products = GetProducts();
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <returns>An array of products. An empty array if no product exists.</returns>
+        /// <exception cref="Exception">
+        /// </exception>
+        // Copies all products from the database into an array
         public Product[] GetProducts()
-        {   
+        {
             try
             {
                 Product[] products = _developDbContext.Products.ToArray();
