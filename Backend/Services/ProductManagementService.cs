a553809fa1b1371bcb38cb5c98f229175edb2968
Kishan J Patel <kishan.patel2002@outlook.com>
2023-10-28 14:53:57 -0600

    feat: Add product management service

---
+++ Backend/Services/ProductManagementService.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Common;
+using Backend.Data;
+using Backend.Models;
+
+namespace Backend.Services
+{
+    public class ProductManagementService
+    {
+        public ProductManagementService(OssDbContext context)
+        {
+            _developDbContext = context;
+        }
+
+        private readonly OssDbContext _developDbContext;
+
+        public void SaveProduct(Product product)
+        {
+            try
+            {
+                _developDbContext.Products.Add(product);
+                _developDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Product[] GetProducts()
+        {   
+            try
+            {
+                Product[] products = _developDbContext.Products.ToArray();
+                return products;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
